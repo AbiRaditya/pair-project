596b5190b36445e0a6bf110a76ae62950df2bba9
AbiRaditya <abi.radithya@gmail.com>
2021-08-26 05:46:36 +0800

    d

--- sandbox.js
+++
@@ -1,5 +0,0 @@
-// npx sequelize-cli model:generate --name User --attributes profile_picture:string,username:string,password:string,first_name:string,last_name:string,email:string
-// npx sequelize-cli model:generate --name Feed --attributes title:string,content:string
-// npx sequelize-cli model:generate --name Tag --attributes name:string
-// npx sequelize-cli model:generate --name FeedUser --attributes FeedID:integer,UserID:integer
-// npx sequelize-cli model:generate --name FeedTag --attributes FeedID:integer
