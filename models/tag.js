2ab78f2bee01dcb8ee9f2346fdf3e455406a3f6c
AbiRaditya <abi.radithya@gmail.com>
2021-08-26 18:20:41 +0800

    relasi di model feed & tags

--- models/tag.js
+++ models/tag.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Tag.belongsToMany(models.Feed , { through: `FeedTags` , foreignKey: `tagID` })
     }
   };
   Tag.init({
