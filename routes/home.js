39779f6b67b77b2ca27fa8ae0a3ac30717213797
Isella Safira <isellasafira@gmail.com>
2021-08-26 16:06:27 +0700

    test

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,9 @@ const express = require('express')
 const Controller = require('../controllers/controller')
 const app = express()
 const home_router = express.Router()
+const feeds_router = require(`./feeds`)
 
 home_router.get('/', Controller.home)
-
+home_router.use(`/`, feeds_router)
 
 module.exports = home_router
