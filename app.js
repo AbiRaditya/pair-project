49cd6eb8dda8aba875da3aaf5a9b9d8810ce3181
AbiRaditya <abi.radithya@gmail.com>
2021-08-27 09:19:39 +0800

    heroku

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const router = require(`./routes/home`)
 
 
 app.use(express.urlencoded({extended:true}))
-const port = 3000
+const PORT = process.env.PORT|| 3000
 
 app.use(express.static(path.join(__dirname , `public`)))
 app.set(`view engine` , `ejs`)
 app.use(`/` , router)
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
